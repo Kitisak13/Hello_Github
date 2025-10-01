c63de716bedddd8fcb4423d3c4a724e52f95b3e6
Kitisak13 <kitisak.junsong@gmail.com>
2025-10-01 20:02:18 +0700

    Move Hello to def

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
-print("Hello, World!")
-print("This is a simple Python script.")
-print("It prints multiple lines to the console.")
-print("Goodbye, World!")
+# A simple Python script that prints multiple lines to the console
+def hello():
+    print("Hello, World!")
+    print("This is a simple Python script.")
+    print("It prints multiple lines to the console.")
+    print("Goodbye, World!")
+
+hello()
