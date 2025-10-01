b2b9c7e42752b10fad6b43b51595d376cbc0537f
Kitisak13 <kitisak.junsong@gmail.com>
2025-10-01 20:05:28 +0700

    change text

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@ def hello():
     print("Hello, World!")
     print("This is a simple Python script.")
     print("It prints multiple lines to the console.")
-    print("Goodbye, World!")
+    print("Goodbye, My lovers!")
 
 hello()
